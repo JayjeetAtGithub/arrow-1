38c173caad3dcb14ae0b227d015f95e024297952
Sebastiaan Alvarez Rodriguez van Ravensberg <sebastiaanalva@gmail.com>
2021-06-19 16:00:47 +0200

    WIP: Bugfixes to connection cacher implementation.

--- java/dataset/src/main/java/org/apache/arrow/dataset/rados/ConnectionKeeper.java
+++ java/dataset/src/main/java/org/apache/arrow/dataset/rados/ConnectionKeeper.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.arrow.dataset.rados;
 
 import javax.annotation.Nonnull;
@@ -14,7 +31,12 @@ import java.util.concurrent.ConcurrentHashMap;
  * @param <T> Keytype to use for keeping connection.
  * @param <U> Connection-like valuetype to use.
  */
-abstract class ConnectionKeeper<T, U extends Connection> implements Closeable {
+public abstract class ConnectionKeeper<T, U extends Connection> implements Closeable {
+
+    @FunctionalInterface
+    public interface Construct<T, U> {
+        U construct(T t);
+    }
 
     protected ConcurrentHashMap<T, U> map;
     protected final Object putLock = new Object();
@@ -59,31 +81,49 @@ abstract class ConnectionKeeper<T, U extends Connection> implements Closeable {
     }
 
     /**
-     * Gets a connection.
+     * Get a connection.
      * @implNote weak retrieval operations such as this only provide eventual consistency, but are faster than their regular counterparts.
      * @see #get(Object)
      * @param key Key we used to store connection with.
      * @return Connection if found. `null` otherwise.
      */
-    public @Nullable Connection getWeak(T key) {
+    public @Nullable U getWeak(T key) {
         return map.get(key);
     }
 
     /**
-     * Gets a connection.
+     * Get a connection.
      * @implNote strong retrieval operations such as this provide strong consistency, but are slower than their 'weak' counterparts.
      * @see #getWeak(Object)
      * @param key Key we used to store connection with.
      * @return Connection if found. `null` otherwise.
      */
-    public @Nullable Connection get(T key) {
+    public @Nullable U get(T key) {
         synchronized (putLock) {
             return map.get(key);
         }
     }
 
     /**
-     * Removes a connection. This also closes it.
+     * Get a connection, or create.
+     * @param key Key for connection to fetch or insert.
+     * @param construct Construction method to create a new Connection, in case we have to create.
+     * @return the retrieved or created connection.
+     */
+    public U getOrCreate(T key, Construct<T, U> construct) {
+        @Nullable U conn;
+        synchronized (putLock) {
+            conn = map.get(key);
+            if (conn == null) {
+                conn = construct.construct(key);
+                map.put(key, conn);
+            }
+        }
+        return conn;
+    }
+
+    /**
+     * Remove a connection and close it.
      * @param key Key for connection to remove.
      */
     public void remove(T key) throws IOException {
@@ -109,7 +149,7 @@ abstract class ConnectionKeeper<T, U extends Connection> implements Closeable {
      * @return connection with given id if found, `null` otherwise.
      * @implNote If multiple connections exist with given id (should NEVER occur), it is undefined which one is returned.
      */
-    public @Nullable Connection getById(long id) {
+    public @Nullable U getById(long id) {
          Optional<U> conn = map.values().stream().parallel().filter(connection -> connection.id() == id).findAny();
         return conn.orElse(null);
     }
